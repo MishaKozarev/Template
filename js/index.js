d0f6ed052c903a6e7e25f56a785ab6c7f2fc7e6c
MishaKozarev <mishaaaaa1986@mail.ru>
2024-04-05 21:59:00 +0300

    feat: add EventDecor task

--- js/index.js
+++ js/index.js
@@ -1,13 +1,13 @@
+const header = document.querySelector("header");
 const logo = document.querySelector(".header__logo");
 const links = document.querySelectorAll(".menu__link");
 const menu = document.querySelector(".menu");
 const btn = document.querySelector(".burger__btn");
-const dropdown = document.querySelectorAll(".faq__title");
 
 function addBurger() {
-  btn.addEventListener('click', addStyle);
+  btn.addEventListener("click", addStyle);
   links.forEach((link) => {
-    link.addEventListener('click', addStyle);
+    link.addEventListener("click", addStyle);
   });
 }
 function addStyle() {
@@ -16,3 +16,11 @@ function addStyle() {
   logo.classList.toggle("active");
 }
 addBurger();
+
+window.addEventListener("scroll", function () {
+  if (window.scrollY > 350) {
+    header.style.backgroundColor = "rgb(46, 55, 70";
+  } else {
+    header.style.backgroundColor = "";
+  }
+});
