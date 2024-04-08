e6441dd19d2df7817a3a4a5c6c765805cafde38a
MishaKozarev <mishaaaaa1986@mail.ru>
2024-04-08 13:11:12 +0300

    feat: add header and footer

--- send-form.php
+++ send-form.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $toEmail = "fitgym@gmail.com";
+  $toEmail = "primeoffice@gmail.com";
   $subject = "Новое сообщение";
 
   $name = $_POST["name"];
