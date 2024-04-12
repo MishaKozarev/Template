ec1d0a5a663264ac7127dc7bd2f51d37f0647242
MishaKozarev <mishaaaaa1986@mail.ru>
2024-04-12 17:52:22 +0300

    feat: add footer

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 const logo = document.querySelector(".header__logo");
+const headerTop = document.querySelector(".header-top");
+const headerBottom = document.querySelector(".header-bottom");
 const links = document.querySelectorAll(".menu__link");
 const nav = document.querySelector(".nav");
 const btn = document.querySelector(".burger__btn");
@@ -15,3 +17,23 @@ function addStyle() {
   logo.classList.toggle("active");
 }
 addBurger();
+
+window.addEventListener("scroll", function () {
+  var scrollPosition = window.scrollY;
+
+  if (scrollPosition > 100) {
+    headerTop.style.display = "none";
+    headerBottom.style.backgroundColor = "white";
+  } else {
+    headerTop.style.display = "block";
+    headerBottom.style.backgroundColor = "inherit";
+  }
+});
+
+window.addEventListener("resize", function () {
+  if (window.innerWidth <= 700) {
+    headerTop.style.display = "none";
+  } else {
+    headerTop.style.display = "block";
+  }
+});
