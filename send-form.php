e479442e21a0094fb355d6563f2cd024a97be97c
MishaKozarev <mishaaaaa1986@mail.ru>
2024-04-14 21:37:43 +0300

    init: start task

--- send-form.php
+++ send-form.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $toEmail = "impactifymarketing@gmail.com";
+  $toEmail = "puresoaps@gmail.com";
   $subject = "Новое сообщение";
 
   $name = $_POST["name"];
