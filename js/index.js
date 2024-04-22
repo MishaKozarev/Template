312acd3250f9d0fd413c63545da59084a6627198
MishaKozarev <mishaaaaa1986@mail.ru>
2024-04-22 11:36:49 +0300

    feat: add header and footer

--- js/index.js
+++ js/index.js
@@ -1,5 +1,6 @@
 const logo = document.querySelector(".header__logo");
 const links = document.querySelectorAll(".menu__link");
+const linksBtn = document.querySelector(".header__btn-burger");
 const nav = document.querySelector(".nav");
 const btn = document.querySelector(".burger__btn");
 
@@ -8,6 +9,7 @@ function addBurger() {
   links.forEach((link) => {
     link.addEventListener("click", addStyle);
   });
+  linksBtn.addEventListener("click", addStyle);
 }
 function addStyle() {
   btn.classList.toggle("active");
