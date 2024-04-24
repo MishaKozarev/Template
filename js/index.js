b7ff0b0381ce80fcd76da4f4276738f1a92dc6f4
MishaKozarev <mishaaaaa1986@mail.ru>
2024-04-24 14:59:22 +0300

    feat: add faq block

--- js/index.js
+++ js/index.js
@@ -2,6 +2,7 @@ const logo = document.querySelector(".header__logo");
 const links = document.querySelectorAll(".menu__link");
 const nav = document.querySelector(".nav");
 const btn = document.querySelector(".burger__btn");
+const dropdown = document.querySelectorAll(".QNOULduJaRLEuXU");
 
 function addBurger() {
   btn.addEventListener("click", addStyle);
@@ -15,3 +16,18 @@ function addStyle() {
   logo.classList.toggle("active");
 }
 addBurger();
+
+const toggleDropdown = (element) => {
+  element.currentTarget.classList.toggle("faq__visible");
+  const item = element.currentTarget.nextElementSibling;
+  if (element.currentTarget.classList.contains("faq__visible")) {
+    item.style.maxHeight = "350px";
+  } else {
+    item.style.maxHeight = 0;
+  }
+};
+if (dropdown) {
+  dropdown.forEach((element) => {
+    element.addEventListener("click", toggleDropdown);
+  });
+}
