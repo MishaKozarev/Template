710fc0ff964f110e9287009f9bb55bb5f63a028c
MishaKozarev <mishaaaaa1986@mail.ru>
2024-04-26 08:44:02 +0300

    feat: replace icons

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ const header = document.querySelector(".header");
 const links = document.querySelectorAll(".menu__link");
 const nav = document.querySelector(".nav");
 const btn = document.querySelector(".burger__btn");
-const dropdown = document.querySelectorAll(".HdVXiBnxUBZpCqy");
 
 function addBurger() {
   btn.addEventListener("click", addStyle);
@@ -28,6 +27,7 @@ window.addEventListener("scroll", function () {
   }
 });
 
+const dropdown = document.querySelectorAll(".ownyCoLMsyfTEcs");
 const toggleDropdown = (element) => {
   element.currentTarget.classList.toggle("faq__visible");
   const item = element.currentTarget.nextElementSibling;
