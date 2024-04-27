924e301ea1c2ecfeb68103d1acf91f8012f5d552
MishaKozarev <mishaaaaa1986@mail.ru>
2024-04-27 15:02:35 +0300

    fix: fix conflicts

--- send-form.php
+++ send-form.php
@@ -1,10 +1,6 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-<<<<<<< HEAD
   $toEmail = "learnquest@gmail.com";
-=======
-  $toEmail = "brandinggenius @gmail.com";
->>>>>>> main
   $subject = "Новое сообщение";
 
   $name = $_POST["name"];
