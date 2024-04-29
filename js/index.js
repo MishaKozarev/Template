cf8bac8dce7a25f44a660360f52bc060a3e27f8f
MishaKozarev <mishaaaaa1986@mail.ru>
2024-04-29 15:46:35 +0300

    feat: add header

--- js/index.js
+++ js/index.js
@@ -1,8 +1,8 @@
 const logo = document.querySelector(".header__logo");
+const header = document.querySelector(".header");
 const links = document.querySelectorAll(".menu__link");
 const nav = document.querySelector(".nav");
 const btn = document.querySelector(".burger__btn");
-const dropdown = document.querySelectorAll(".QNOULduJaRLEuXU");
 
 function addBurger() {
   btn.addEventListener("click", addStyle);
@@ -17,6 +17,7 @@ function addStyle() {
 }
 addBurger();
 
+const dropdown = document.querySelectorAll(".ownyCoLMsyfTEcs");
 const toggleDropdown = (element) => {
   element.currentTarget.classList.toggle("faq__visible");
   const item = element.currentTarget.nextElementSibling;
