38bd71a249279c19a06586752852aa8cdb45a9e1
MishaKozarev <mishaaaaa1986@mail.ru>
2024-04-29 23:21:22 +0300

    feat: finish task

--- js/index.js
+++ js/index.js
@@ -1,8 +1,9 @@
 const logo = document.querySelector(".header__logo");
+const header = document.querySelector(".header");
 const links = document.querySelectorAll(".menu__link");
 const nav = document.querySelector(".nav");
 const btn = document.querySelector(".burger__btn");
-const dropdown = document.querySelectorAll(".QNOULduJaRLEuXU");
+const dropdown = document.querySelectorAll(".qdHEWLwTjXWRcwP");
 
 function addBurger() {
   btn.addEventListener("click", addStyle);
@@ -16,6 +17,16 @@ function addStyle() {
   logo.classList.toggle("active");
 }
 addBurger();
+
+window.addEventListener("scroll", function () {
+  var scrollPosition = window.scrollY;
+
+  if (scrollPosition > 100) {
+    header.style.borderBottom = "1px solid #e3655b";
+  } else {
+    header.style.borderBottom = "1px solid white";
+  }
+});
 
 const toggleDropdown = (element) => {
   element.currentTarget.classList.toggle("faq__visible");
