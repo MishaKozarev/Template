fd877f67bffaa6b35396baaccfea1431749b6e10
MishaKozarev <mishaaaaa1986@mail.ru>
2024-05-02 22:11:03 +0300

    feat: replace icons and pictures

--- js/index.js
+++ js/index.js
@@ -1,9 +1,9 @@
 const logo = document.querySelector(".header__logo");
-const headerTop = document.querySelector(".header-top");
-const headerBottom = document.querySelector(".header-bottom");
+const header = document.querySelector(".header");
 const links = document.querySelectorAll(".menu__link");
 const nav = document.querySelector(".nav");
 const btn = document.querySelector(".burger__btn");
+const dropdown = document.querySelectorAll(".haBeTIYHHCWOgLe");
 
 function addBurger() {
   btn.addEventListener("click", addStyle);
@@ -18,17 +18,6 @@ function addStyle() {
 }
 addBurger();
 
-window.addEventListener("scroll", function () {
-  var scrollPosition = window.scrollY;
-
-  if (scrollPosition > 100) {
-    headerBottom.style.backgroundColor = "#fff";
-  } else {
-    headerBottom.style.backgroundColor = "#fff";
-  }
-});
-
-const dropdown = document.querySelectorAll(".UBCrmJXqXCHeAEt");
 const toggleDropdown = (element) => {
   element.currentTarget.classList.toggle("faq__visible");
   const item = element.currentTarget.nextElementSibling;
