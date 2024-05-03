50a6da85ff0c37c25b614da6c0611775f1d0d173
MishaKozarev <mishaaaaa1986@mail.ru>
2024-05-03 16:38:09 +0300

    init: start task

--- send-form.php
+++ send-form.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $toEmail = "powerpulse@gmail.com";
+  $toEmail = "culinarycreations@gmail.com";
   $subject = "Новое сообщение";
 
   $name = $_POST["name"];
