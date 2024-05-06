9773137e0563c89ac520f583a77243d7d87afdb5
MishaKozarev <mishaaaaa1986@mail.ru>
2024-05-07 01:09:56 +0300

    feat: replace text

--- js/index.js
+++ js/index.js
@@ -17,7 +17,7 @@ function addStyle() {
 }
 addBurger();
 
-const dropdown = document.querySelectorAll(".BXsqhyMXcvGMNwZ");
+const dropdown = document.querySelectorAll(".GtIrJVeXmhwUwgx");
 const toggleDropdown = (element) => {
   element.currentTarget.classList.toggle("faq__visible");
   const item = element.currentTarget.nextElementSibling;
