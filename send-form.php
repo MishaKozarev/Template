ab3ff753fb695b8590ce7907d6d1b6e48239b930
MishaKozarev <mishaaaaa1986@mail.ru>
2024-05-07 22:19:11 +0300

    feat: add info pages

--- send-form.php
+++ send-form.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $toEmail = "codecraft @gmail.com";
+  $toEmail = "codecraft@gmail.com";
   $subject = "Новое сообщение";
 
   $name = $_POST["name"];
