9660d30ffde849c048c9bd1e89d890a14b9dc3f5
MishaKozarev <mishaaaaa1986@mail.ru>
2024-05-14 00:51:29 +0300

    feat: add header and footer

--- send-form.php
+++ send-form.php
@@ -1,6 +1,6 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-  $toEmail = "peakroute @gmail.com";
+  $toEmail = "peakroute@gmail.com";
   $subject = "Новое сообщение";
 
   $name = $_POST["name"];
