da1e6660680dce1d19ab5e36f695384a864dd422
MishaKozarev <mishaaaaa1986@mail.ru>
2024-05-17 01:51:20 +0300

    feat: add header

--- js/index.js
+++ js/index.js
@@ -3,7 +3,6 @@ const header = document.querySelector(".header");
 const links = document.querySelectorAll(".menu__link");
 const nav = document.querySelector(".nav");
 const btn = document.querySelector(".burger__btn");
-const dropdown = document.querySelectorAll(".ktrVYtbnDifFtOS");
 
 function addBurger() {
   btn.addEventListener("click", addStyle);
@@ -18,25 +17,14 @@ function addStyle() {
 }
 addBurger();
 
-window.addEventListener("scroll", function () {
-  var scrollPosition = window.scrollY;
-
-  if (scrollPosition > 100) {
-    header.style.borderBottom = "1px solid #a88343";
-  } else {
-    header.style.borderBottom = "1px solid #f2ede6";
-  }
-});
-
+const dropdown = document.querySelectorAll(".KtynUAwTkyZUVvw");
 const toggleDropdown = (element) => {
   element.currentTarget.classList.toggle("faq__visible");
   const item = element.currentTarget.nextElementSibling;
   if (element.currentTarget.classList.contains("faq__visible")) {
     item.style.maxHeight = "350px";
-    item.style.display = "block";
   } else {
     item.style.maxHeight = 0;
-    item.style.display = "none";
   }
 };
 if (dropdown) {
