05f2b9ec1977f2f278f3705c3c3261a0a2fbbb18
MishaKozarev <mishaaaaa1986@mail.ru>
2024-05-21 09:40:20 +0300

    fix: fix branch

--- send-form.php
+++ send-form.php
@@ -1,14 +1,6 @@
 <?php
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
-<<<<<<< HEAD
   $toEmail = "aquafit@gmail.com";
-=======
-<<<<<<< HEAD
-  $toEmail = "thrivegreenhouse@gmail.com";
-=======
-  $toEmail = "sunpower@gmail.com";
->>>>>>> main
->>>>>>> main
   $subject = "Новое сообщение";
 
   $name = $_POST["name"];
